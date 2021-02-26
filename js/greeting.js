d352a472727e1ca285fd1908592fd9ebde9f05ae
ajekt <ajekt@outlook.com>
2021-02-26 21:05:28 +0800

    Customizations

--- js/greeting.js
+++ js/greeting.js
@@ -3,23 +3,13 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+// const name = ' John Doe';
 
 // Here you can change your greetings
-const gree1 = 'Go to Sleep!  ';
-const gree2 = 'Good morning!  ';
-const gree3 = 'Good afternoon  ';
-const gree4 = 'Good evening,  ';
-const gree5 = 'Good evening,  ';
-const gree6 = 'Good evening,  ';
+const greetings = ['Go to Sleep!  ', 'Good morning!  ', 'Good afternoon  ', 'Good Evening  '];
 
 // Define the hours of the greetings
-if (hour >= 23 && hour < 5) {
-  document.getElementById('greetings').innerText = gree1 + name;
-} else if (hour >= 6 && hour < 12) {
-  document.getElementById('greetings').innerText = gree2 + name;
-} else if (hour >= 12 && hour < 17) {
-  document.getElementById('greetings').innerText = gree3 + name;
-} else {
-  document.getElementById('greetings').innerText = gree4 + name;
-}
+if (hour >= 23 && hour < 5) document.getElementById('greetings').innerText = greetings[0]; else
+if (hour >= 6 && hour < 12) document.getElementById('greetings').innerText = greetings[1]; else
+if (hour >= 12 && hour < 17) document.getElementById('greetings').innerText = greetings[2]; else
+document.getElementById('greetings').innerText = greetings[3];
